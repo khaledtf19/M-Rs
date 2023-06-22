f5f0351ac7601617e5a0634239f8cab607b33923
Khaledtf Labeb <khaledtf19@gmail.com>
2023-06-22 18:41:36 +0300

    adding Layout

---
+++ src/components/Layouts/Layout.tsx
@@ -0,0 +1,5 @@
+const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  return <main>{children}</main>;
+};
+
+export default Layout;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,16 @@
 import { type AppType } from "next/app";
-import {
-  ClerkProvider,
-} from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 import { api } from "../utils/api";
 
 import "../styles/globals.css";
+import Layout from "~/components/Layouts/Layout";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-
   return (
     <ClerkProvider {...pageProps}>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ClerkProvider>
   );
 };
