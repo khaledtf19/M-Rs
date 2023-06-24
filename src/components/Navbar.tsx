d42139a6bf51a8eca04b4471a77263ec60ecb108
Khaledtf Labeb <khaledtf19@gmail.com>
2023-06-24 20:43:19 +0300

    adding main nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { useTheme } from "next-themes";
 import Link from "next/link";
 import React from "react";
 
-import { Button } from "./ui/button";
+import { Button, buttonVariants } from "./ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -14,60 +14,78 @@ import {
 } from "./ui/dropdown-menu";
 import {
   NavigationMenu,
-  NavigationMenuContent,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
 } from "./ui/navigation-menu";
 
 const Navbar = () => {
-  const { setTheme } = useTheme();
   return (
-      <div className="container">
-        <NavigationMenu>
-          <NavigationMenuList>
-            <NavigationMenuItem>
-              <Link href="/">Home</Link>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <Link href="/hello">Hello</Link>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-          <NavigationMenuList>
-            <NavigationMenuItem>
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <Button variant="outline" size="icon">
-                    <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                    <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                    <span className="sr-only">Toggle theme</span>
-                  </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent align="end">
-                  <DropdownMenuItem onClick={() => setTheme("light")}>
-                    Light
-                  </DropdownMenuItem>
-                  <DropdownMenuItem onClick={() => setTheme("dark")}>
-                    Dark
-                  </DropdownMenuItem>
-                  <DropdownMenuItem onClick={() => setTheme("system")}>
-                    System
-                  </DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
-      </div>
+    <div className="container sticky py-2 top-0 right-0 left-0 z-50">
+      <MainNav />
+      <MobileNav />
+    </div>
   );
 };
 
 export default Navbar;
 
 const MainNav = () => {
-  return;
+  const { setTheme } = useTheme();
+  return (
+    <NavigationMenu className="flex justify-between">
+      <NavigationMenuList>
+        <LinkButton href="/" text="Home" />
+        <LinkButton href="/movies" text="Movies" />
+        <LinkButton href="/reviews" text="Reviews" />
+      </NavigationMenuList>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button
+                className={navigationMenuTriggerStyle()}
+                variant={"ghost"}
+                size="icon"
+              >
+                <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                <span className="sr-only">Toggle theme</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuItem onClick={() => setTheme("light")}>
+                Light
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setTheme("dark")}>
+                Dark
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setTheme("system")}>
+                System
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
 };
 const MobileNav = () => {
-  return;
+  return <></>;
+};
+
+const LinkButton: React.FC<{ href: string; text: string }> = ({
+  href,
+  text,
+}) => {
+  return (
+    <NavigationMenuItem>
+      <Link href={href}>
+        <NavigationMenuLink className={buttonVariants({ variant: "link" })}>
+          {text}
+        </NavigationMenuLink>
+      </Link>
+    </NavigationMenuItem>
+  );
 };
