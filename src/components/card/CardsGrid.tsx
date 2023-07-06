dd65b010155ee23ad29438661134a11f122f1932
Khaledtf Labeb <khaledtf19@gmail.com>
2023-07-06 21:58:14 +0300

    update cards grid, dark theme

--- src/components/card/CardsGrid.tsx
+++ src/components/card/CardsGrid.tsx
@@ -1,14 +1,35 @@
+import Image from "next/image";
 import { CardType } from "~/types/utils";
+import { SmallPoster } from "~/utils/utils";
 
-import { Card, CardContent, CardHeader } from "../ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
 
 const CardsGrid: React.FC<{ cards: CardType[] }> = ({ cards }) => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4  gap-5">
+    <div className="grid grid-cols-1 items-center content-center place-items-center sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 w-full gap-5">
       {cards.map((card) => (
-        <Card key={card.id} className=" w-56">
-          <CardContent>{card.description}</CardContent>
-          <CardHeader>{card.title}</CardHeader>
+        <Card key={card.id} className=" w-56 justify-center ">
+          <CardHeader className="p-0">
+            <div className="relative  h-80  ">
+              <Image
+                alt={card.title}
+                className="rounded-t-md"
+                fill
+                src={SmallPoster + card.image}
+                sizes="(max-width: 500px) 100px, (max-width: 900px) 200px, (max-width: 1200px) 300px, "
+              />
+            </div>
+          </CardHeader>
+          <CardContent>
+            <CardTitle>{card.title}</CardTitle>
+            <CardDescription>{card.description}</CardDescription>
+          </CardContent>
         </Card>
       ))}
     </div>
