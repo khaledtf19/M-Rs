76948311cd2d6d4d154ec16cfd3ebcad7660c8b5
Khaledtf Labeb <khaledtf19@gmail.com>
2023-07-08 21:08:57 +0300

    adding search type

--- src/server/api/routers/search.ts
+++ src/server/api/routers/search.ts
@@ -1,8 +1,28 @@
 import { z } from "zod";
+import { SearchResultsType } from "~/types/utils";
+
 import { createTRPCRouter, publicProcedure } from "../trpc";
 
 export const searchRouter = createTRPCRouter({
-  search: publicProcedure.input(z.object({ query: z.string() })).query(({ input }) => {
-    return input.query; 
-  })
-})
+  search: publicProcedure
+    .input(
+      z.object({
+        query: z.string(),
+        curser: z.number().nullish(),
+      })
+    )
+    .query(async ({ input }) => {
+      let { query, curser } = input;
+      curser = curser ?? 1;
+
+      const response = await fetch(
+        `https://api.themoviedb.org/3/search/multi?query=${query}&include_adult=false&language=en-US&page=${curser}`
+      );
+      const data = (await response.json()) as SearchResultsType;
+
+      return {
+        nextPage: curser + 1,
+        maxPages: data.total_pages,
+      };
+    }),
+});
--- src/types/utils.ts
+++ src/types/utils.ts
@@ -4,3 +4,32 @@ export type CardType = {
   image: string;
   description: string;
 };
+
+export type SearchResultsType = {
+  page: number
+  results: Result[]
+  total_pages: number
+  total_results: number
+}
+
+export type Result = {
+  title?: string
+  name?: string
+  adult: boolean
+  backdrop_path?: string
+  id: number
+  original_name?: string
+  original_language: string
+  overview: string
+  poster_path?: string
+  media_type: "tv"| "movie" 
+  genre_ids: number[]
+  popularity: number
+  first_air_date?: string
+  vote_average: number
+  vote_count: number
+  origin_country?: string[]
+  original_title?: string
+  release_date?: string
+  video?: boolean
+}
