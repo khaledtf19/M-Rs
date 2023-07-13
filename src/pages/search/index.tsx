4228083e9407fc867fcf1ce41ad447efb3f9ef60
Khaledtf Labeb <khaledtf19@gmail.com>
2023-07-13 19:57:19 +0300

    update search to useMut

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CardsGrid from "~/components/card/CardsGrid";
 import { Button } from "~/components/ui/button";
 import { Input } from "~/components/ui/input";
@@ -6,20 +6,21 @@ import { api } from "~/utils/api";
 
 const SearchPage: React.FC = () => {
   const [search, setSearch] = useState("");
-  const [currentPage, setCurrentPage] = useState(0);
 
-  const Q = api.search.search.useInfiniteQuery(
-    {
-      query: search,
-    },
-    {
-      getNextPageParam: (lastPage) =>
-        lastPage.maxPages < currentPage + 1 ? undefined : lastPage.nextPage,
-      getPreviousPageParam: (lastPage) =>
-        currentPage === 0 ? undefined : lastPage.prevPage,
-      initialCursor: 1,
-    }
-  );
+  const [currentPage, setCurrentPage] = useState(1);
+
+
+  const {data, isLoading, mutate: mutateSearch} = api.search.search.useMutation({})
+  useEffect(() => {
+    let timer = setTimeout(() => {
+      mutateSearch({ query: search, page: 1}) 
+    }, 1500);
+    return () => clearTimeout(timer);
+  }, [search]);
+
+  useEffect(() => {
+    console.log("currentPage", currentPage);
+  }, [currentPage]);
 
   return (
     <div className="flex flex-col items-center justify-center gap-5">
@@ -29,31 +30,36 @@ const SearchPage: React.FC = () => {
           placeholder="Title..."
           value={search}
           onChange={(e) => {
-            setSearch(e.target.value);
-            setCurrentPage(0);
+            setSearch((prevstate) => {
+              if (prevstate !== e.target.value) {
+                console.log(e.target.value, "here");
+                setCurrentPage(1);
+              }
+              return e.target.value;
+            });
           }}
         />
       </div>
-      <CardsGrid cards={Q.data?.pages[currentPage]?.results} />
-      {Q.isFetching && <div>Loading...</div>}
+      <CardsGrid cards={data?.results} />
+      {isLoading && <div>Loading...</div>}
       <div>
         <Button
-          onClick={() => {
-            Q.fetchNextPage();
+          onClick={async () => {
+            mutateSearch({ query: search, page: currentPage + 1 });
             setCurrentPage((prevPage) => prevPage + 1);
           }}
           variant={"outline"}
-          disabled={!Q.hasNextPage}
+          /* disabled={!Q.hasNextPage} */
         >
           Next Page
         </Button>
         <Button
-          onClick={() => {
-            Q.fetchPreviousPage();
+          onClick={async () => {            
+            mutateSearch({ query: search, page: currentPage - 1 });
             setCurrentPage((prevPage) => prevPage - 1);
           }}
           variant={"outline"}
-          disabled={!Q.hasPreviousPage}
+          // disabled={!Q.hasPreviousPage}
         >
           Prev Page
         </Button>
--- src/server/api/routers/search.ts
+++ src/server/api/routers/search.ts
@@ -6,17 +6,11 @@ import { createTRPCRouter, publicProcedure } from "../trpc";
 
 export const searchRouter = createTRPCRouter({
   search: publicProcedure
-    .input(
-      z.object({
-        query: z.string(),
-        cursor: z.number().nullish(),
-      })
-    )
-    .query(async ({ input }) => {
-      const cursor = input.cursor ?? 1;
-
+    .input(z.object({ query: z.string(), page: z.number(), 
+  }))
+    .mutation(async ({ input }) => {
       const response = await fetch(
-        `https://api.themoviedb.org/3/search/multi?query=${input.query}&include_adult=false&language=en-US&page=${cursor}`,
+        `https://api.themoviedb.org/3/search/multi?query=${input.query}&include_adult=false&language=en-US&page=${input.page}`,
         {
           headers: {
             contentType: "application/json",
@@ -25,15 +19,7 @@ export const searchRouter = createTRPCRouter({
         }
       );
       const data = (await response.json()) as SearchResultsType;
-    console.log(data)
-         if (!data||data.results.length === 0)
-        return {
-          results: undefined,
-          nextPage: undefined,
-          prevPage: undefined,
-          maxPages: 0,
-        };
-      const results = data.results.map((movie) => ({
+    const results = data?.results?.map((movie) => ({
         id: movie.id,
         title: movie.title || movie.name,
         image: movie.poster_path,
@@ -42,11 +28,8 @@ export const searchRouter = createTRPCRouter({
       })) as CardType[];
 
       return {
-        curser: cursor,
-        nextPage: cursor + 1,
-        prevPage: cursor - 1,
         maxPages: data.total_pages,
         results: results,
-      };
+    }
     }),
 });
